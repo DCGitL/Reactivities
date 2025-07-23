96617d380c231d68bfe7bc83e207d556fb20c1cf
David Chen <davidl5a@hotmail.com>
2025-07-23 10:47:02 -0400

    End of section 18 update ActivityForm

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -114,7 +114,11 @@ export default function ActivityForm() {
 					display={"flex"}
 					justifyContent="end"
 					gap={3}>
-					<Button color="inherit">Cancel</Button>
+					<Button
+						onClick={() => navigate(-1)}
+						color="inherit">
+						Cancel
+					</Button>
 					<Button
 						type="submit"
 						color="success"
